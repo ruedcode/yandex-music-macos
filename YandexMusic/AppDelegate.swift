7cf31c68a52c64c778ef083b5050635ac8bb08e5
Eugene Kalyada <gener@inbox.ru>
2021-11-27 12:58:19 +0300

    Initial Commit

---
+++ YandexMusic/AppDelegate.swift
@@ -0,0 +1,51 @@
+//
+//  AppDelegate.swift
+//  SwiftUIMenuBar
+//
+//  Created by Aaron Wright on 12/18/19.
+//  Copyright © 2019 Aaron Wright. All rights reserved.
+//
+
+import Cocoa
+import SwiftUI
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    var popover: NSPopover!
+    var statusBarItem: NSStatusItem!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Create the SwiftUI view that provides the window contents.
+        let contentView = ContentView()
+
+        // Create the popover
+        let popover = NSPopover()
+        popover.contentSize = NSSize(width: 400, height: 400)
+        popover.behavior = .transient
+        popover.contentViewController = NSHostingController(rootView: contentView)
+        self.popover = popover
+        
+        // Create the status item
+        self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
+        
+        if let button = self.statusBarItem.button {
+            button.image = NSImage(named: "Music")
+            button.action = #selector(togglePopover(_:))
+        }
+        
+        NSApp.activate(ignoringOtherApps: true)
+    }
+    
+    @objc func togglePopover(_ sender: AnyObject?) {
+        if let button = self.statusBarItem.button {
+            if self.popover.isShown {
+                self.popover.performClose(sender)
+            } else {
+                self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+            }
+        }
+    }
+    
+}
+
